9017c0f85d505797f43594367e6478d31bb76dbc
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-02-24 10:42:23 +0700

    Change env call exports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,6 @@
-import("dotenv").then((dotenv) => {
-  dotenv.config();
-});
-
-export default {
-  PORT: process.env.PORT,
-  DBURI: process.env.MONGO_EXPRESS_STRING,
-  DOMAIN: process.env.DOMAIN,
-  JWT_SECRET: process.env.JWT_SECRET,
-};
+import dotenv from "dotenv";
+dotenv.config();
+export const PORT = process.env.PORT;
+export const DBURI = process.env.MONGO_EXPRESS_STRING;
+export const DOMAIN = process.env.DOMAIN;
+export const JWT_SECRET = process.env.JWT_SECRET;
